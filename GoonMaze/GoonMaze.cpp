1aa82bfd5d89e3f3b99f377c9fa8180667211b8c
dkpeders <dkpeders@umich.edu>
2014-02-03 17:36:11 -0500

    Expansion
    Entering the scene are Vango and Das Rippin in this expansion of the
    Gooniest Game on the web!

--- GoonMaze/GoonMaze.cpp
+++ GoonMaze/GoonMaze.cpp
@@ -95,11 +95,113 @@ int main()
 			cin >> input;
 			if (input == 0)
 			{
-				cout << "" << endl; // solution for front door
+				cout << "You open the door to complete clutter" << endl;
+				cout << "Comics, magic cards and various board games lay on the floor" << endl;
+				cout << "0) Pick up your favorite comic and enjoy" << endl;
+				cout << "1) Build the deck of your dreams with what lie on the floor" << endl;
+				cin >> input;
+				if (input == 0)
+				{
+					cout << "Your reading is abruptly ended by a peculiar noise" << endl;
+					cout << "You hear someone breathing" << endl;
+					cout << "0) Maybe it is a Goon! Follow the noise!" << endl;
+					cout << "1) Avoid the noise. It could be whoever destroyed the place" << endl;
+					cin >> input;
+				}
+				else if (input == 1)
+				{
+					cout << "All that you can find is Mogg Flunkies! Man did we over stock" << endl;
+					cout << "Amongst the searching you come across a card that has writing" << endl;
+					cout << "It is a Jace, The Mind Sculptor. Written on it is 'closet'." << endl;
+					cout << "All of a sudden you hear a truck pull up" << endl;
+					cout << "0) Dive behind counter" << endl;
+					cout << "1) Approach the door" << endl;
+					cin >> input;
+					if (input == 0)
+					{
+						cout << "You hear someone enter the building." << endl;
+						cout << "In a panic, you reach for a near by piece of class." << endl;
+						cout << "It is sharp on one end" << endl;
+						cout << "0) Stand up and throw the shard at approaching person" << endl;
+						cout << "1) Walk behind the length of the counter toward the door" << endl;
+						cin >> input;
+						if (input == 0)
+						{
+							cout << "Fuck. You killed Hunter you dumbass." << endl;
+							cout << "Youre worse than Ethan." << endl;
+							cout << "You leave immediately in shock and hide in your house" << endl;
+							cout << "Weeks later youre smothered in your sleep by a person unknown" << endl;
+						}
+						else if (input == 1)
+						{
+							cout << "You see a truck parked by the front door, still running" << endl;
+							cout << "0) Run and steal the truck" << endl;
+							cout << "1) " << endl; // think of progression
+						}
+					}
+					else if (input == 1)
+					{
+						cout << "Congratulations! You found Das Rippin!" << endl;
+						cout << "Good thing he is always late!" << endl;
+						cout << "You fill Hunter in on what has happened. You two brainstorm 2 solutions" << endl;
+						cout << "0) Get in the truck and bail!" << endl;
+						cout << "1) Enter HQ to incase the others are still inside" << endl;
+						cin >> input;
+						if (input == 0)
+						{
+							cout << "You fail. TGK or Die, not TGK or ride." << endl;
+							cout << "You are demoted to Ethan status and excommunicated" << endl;
+						}
+						else if (input == 1)
+						{
+							cout << "You and Rippin enter the building" << endl;
+							cout << "The two of you come across a dilemma" << endl;
+							cout << "0) " << endl; // think of progression
+							cout << "1) " << endl; // think of progression
+							cin >> input;
+						}
+					}
+				}
 			}
 			else if (input == 1)
 			{
-				cout << "" << endl; // solution for back door
+				cout << "You reach the back door to see that it has been kicked open" << endl;
+				cout << "As you walk in you notice that the safe has been cleaned out" << endl;
+				cout << "0) Enter the back office" << endl;
+				cout << "1) Open the door to the stairwell" << endl;
+				cin >> input;
+				if (input == 0)
+				{
+					cout << "You walk in to find the computer has been left on" << endl;
+					cout << "Whoever was here last was looking up prices on Avacyn Angel of Hope" << endl;
+					cout << "0) Da Beard was here!" << endl;
+					cout << "1) Ry Guy was here!" << endl;
+					cin >> input;
+					if (input == 0)
+					{
+						cout << "Bro, do you even Goon?" << endl;
+					}
+					else if (input == 1)
+					{
+						cout << "Yes! He might still be here, but the bloody chairs shows that he is hurt" << endl;
+						cout << "Wait... What is that noise.." << endl;
+						cout << "You hear what resembles snoring coming from the break room" << endl;
+						cout << "that can only mean 1 thing....." << endl;
+						cout << "0) Someone left an african documentary on" << endl;
+						cout << "1) Someone left storms on" << endl;
+						cin >> input;
+						if (input == 0)
+						{
+							cout << "You run into the break room yelling but all of a sudden" << endl;
+							cout << "a Bear stops you ): It was those fags who did this" << endl;
+							cout << " the Bear says 'There you are! Die like your brothers!'" << endl;
+							cout << "He pulls a knife and lunges for you" << endl;
+							cout << "Vango charges and stabs him in the heart." << endl;
+							cout << "You feel a sort of sick satisfaction as the Bear fights for air and drops" << endl;
+							cout << "" << endl; // think of progression
+						}
+					}
+				}
 			}
 		}
 		else
